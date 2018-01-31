4bbc1958452af43376cfca64a8b1b0f2e4db64a2
DGemmell <debbiecarden@hotmail.co.uk>
2018-01-31 12:33:29 +0000

    add drinks test

--- specs/drink_spec.rb
+++ specs/drink_spec.rb
@@ -6,12 +6,17 @@ require_relative ("../drink")
 class DrinkTest < MiniTest::Test
 
   def setup
-    @drink = Drink.new("Vodka")
+    @drink = Drink.new("Vodka", 5)
 
   end
 
   def test_get_name
     assert_equal("Vodka", @drink.name)
+  end
+
+  def test_get_price
+    assert_equal(5, @drink.price)
+
   end
 
 end
